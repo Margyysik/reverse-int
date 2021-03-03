ab0b72ea2d370e9a9d7ca9f57973c168b5996152
Margyysik <71218459+Margyysik@users.noreply.github.com>
2021-03-03 13:08:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let result = n.toString().split('').reverse().join('')
+	if (result[result.length-1]=='-'){
+		return +result.slice(0,result.length-1)
+	} else{
+		return +result
+	}
 }
